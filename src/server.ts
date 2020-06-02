8a3fa994c101a6dda3d903d1eebf2a3b6207078c
tmowes <t-mowes@hotmail.com>
2020-06-02 18:18:29 -0300

    fisrt commit

---
+++ src/server.ts
@@ -0,0 +1,8 @@
+/* eslint-disable no-console */
+import express from 'express'
+
+const app = express()
+app.use(express.json())
+app.listen(3333, () => {
+  console.log('    🚀 Server started on port 3333!   ')
+})
