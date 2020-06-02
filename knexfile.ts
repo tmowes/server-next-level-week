995da54044862072b52b35c957d2293811d9fc62
tmowes <t-mowes@hotmail.com>
2020-06-02 19:42:35 -0300

    add knex migrations

---
+++ knexfile.ts
@@ -0,0 +1,24 @@
+import path from 'path'
+
+module.exports = {
+  client: 'sqlite3',
+  connection: {
+    filename: path.resolve(
+      __dirname,
+      'src',
+      'shared',
+      'database',
+      'database.sqlite',
+    ),
+  },
+  migrations: {
+    directory: path.resolve(
+      __dirname,
+      'src',
+      'shared',
+      'database',
+      'migrations',
+    ),
+  },
+  useNullAsDefault: true,
+}
--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,8 +1,30 @@
 /* eslint-disable no-console */
-import express from 'express'
+import express, { Request, Response, NextFunction } from 'express'
+import cors from 'cors'
+
+import AppError from '@shared/errors/AppError'
+import routes from './routes'
 
 const app = express()
+
+app.use(cors())
 app.use(express.json())
+app.use(routes)
+
+app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
+  if (err instanceof AppError) {
+    return response.status(err.statusCode).json({
+      status: 'error',
+      message: err.message,
+    })
+  }
+  console.log(err)
+  return response.status(500).json({
+    status: 'error',
+    message: 'Internal server error',
+  })
+})
+
 app.listen(3333, () => {
   console.log('    🚀 Server started on port 3333!   ')
 })
