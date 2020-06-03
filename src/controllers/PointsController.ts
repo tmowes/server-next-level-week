9497f9e2d94ea563b592910d2ed29dee88327d96
tmowes <t-mowes@hotmail.com>
2020-06-02 21:28:48 -0300

    create finish controllers

--- src/controllers/PointsController.ts
+++ src/controllers/PointsController.ts
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 /* eslint-disable camelcase */
 /* eslint-disable @typescript-eslint/explicit-module-boundary-types */
 import { Request, Response } from 'express'
@@ -35,6 +36,38 @@ export default class PointsController {
       }
     })
     await trx('points_items').insert(pointsItems)
+    await trx.commit()
     return response.json({ id: point_id, ...point })
+  }
+
+  async index(request: Request, response: Response) {
+    const { city, uf, items } = request.query
+
+    const parsedItems = String(items)
+      .split(',')
+      .map(item => Number(item.trim()))
+
+    const points = await knex('points')
+      .join('points_items', 'points.id', '=', 'points_items.point_id')
+      .whereIn('points_items.item_id', parsedItems)
+      .where('city', String(city))
+      .where('uf', String(uf))
+      .distinct()
+      .select('points.*')
+
+    return response.json(points)
+  }
+
+  async show(request: Request, response: Response) {
+    const { id } = request.params
+    const point = await knex('points').where('id', id).first()
+    if (!point) {
+      return response.status(400).json({ message: 'Point not found.' })
+    }
+    const items = await knex('items')
+      .join('points_items', 'items.id', '=', 'points_items.item_id')
+      .where('points_items.point_id', id)
+      .select('items.title')
+    return response.json({ point, items })
   }
 }
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -11,5 +11,7 @@ const pointsController = new PointsController()
 routes.get('/items', itemsController.index)
 
 routes.post('/points', pointsController.create)
+routes.get('/points', pointsController.index)
+routes.get('/points/:id', pointsController.show)
 
 export default routes
